a24adc21cef7a997881c7d5599232c71df6b270a
Carlos Santos <57176147809@escola928.edu>
2025-10-22 09:18:31 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'produtoIntegrador',
+  webDir: 'www'
+};
+
+export default config;
